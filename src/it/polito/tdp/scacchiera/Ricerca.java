0bf9f4ad5623f1799835e0c2f0aa5b8f1c689fa4
Fulvio Corno <fulvio.corno@polito.it>
2016-04-05 15:48:31 +0200

    Esercizio svolto in aula

---
+++ src/it/polito/tdp/scacchiera/Ricerca.java
@@ -0,0 +1,86 @@
+package it.polito.tdp.scacchiera;
+
+public class Ricerca {
+
+	int conta;
+
+	public int contaSoluzioni(int lato) {
+
+		Scacchiera sc = new Scacchiera(lato);
+
+		conta = 0;
+
+		int fatto = 0; // quante posizioni sono già riempite
+
+		cerca(sc, fatto);
+
+		return conta;
+	}
+
+	private void cerca(Scacchiera sc, int fatto) {
+
+		if (fatto == sc.size()) {
+			// soluzione trovata!!!
+			this.conta++;
+
+			System.out.println("\nTrovata " + conta);
+
+			for (Pos p : sc.getPosizioni())
+				System.out.format("%d %d -> %d\n", p.getRiga(), p.getCol(), sc.get(p));
+
+		} else {
+
+			Pos p = sc.getPosizioni().get(fatto);
+
+			if (tentativoValido(sc, p, 0)) {
+				sc.set(p, 0);
+				cerca(sc, fatto + 1);
+				sc.delete(p);
+			}
+
+			if (tentativoValido(sc, p, 1)) {
+				sc.set(p, 1);
+				cerca(sc, fatto + 1);
+				sc.delete(p);
+			}
+
+			if (tentativoValido(sc, p, 2)) {
+				sc.set(p, 2);
+				cerca(sc, fatto + 1);
+				sc.delete(p);
+			}
+
+		}
+	}
+	
+	private boolean tentativoValido(Scacchiera sc, Pos p, Integer i) {
+		
+		
+		// alto
+		Pos alto = new Pos(p.getRiga()-1, p.getCol()) ;
+		Integer numAlto = sc.get(alto) ;
+		if(numAlto !=  null && Math.abs(numAlto-i)!=1 )
+			return false ;
+		
+		// sinistra
+		Pos sx = new Pos(p.getRiga(), p.getCol()-1) ;
+		Integer numSx = sc.get(sx) ;
+		if(numSx !=  null && Math.abs(numSx-i)!=1 )
+			return false ;
+
+		return true ;
+	}
+
+	public static void main(String[] args) {
+		Ricerca r = new Ricerca();
+
+		r.contaSoluzioni(2);
+		r.contaSoluzioni(3);
+		
+		r.contaSoluzioni(5);
+
+
+
+	}
+
+}
---
+++ src/it/polito/tdp/scacchiera/Scacchiera.java
@@ -0,0 +1,66 @@
+package it.polito.tdp.scacchiera;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scacchiera {
+	
+	private int N ;
+	
+	private Map<Pos,Integer> caselle ;
+	private List<Pos> posizioni ;
+
+	/** 
+	 * Crea una scacchiera vuota di lato N
+	 * @param n
+	 */
+	public Scacchiera(int n) {
+		N = n;
+		caselle = new HashMap<Pos,Integer>() ;
+		
+		// definisci le posizioni valide
+		posizioni = new ArrayList<Pos>() ;
+		for( int riga = 1 ; riga <= N; riga++ ) {
+			for( int col = 1; col <= N; col++ ) {
+				posizioni.add(new Pos(riga, col)) ;
+			}
+		}
+		
+	}
+	
+	public Integer get(Pos p) {
+		return caselle.get(p) ;
+	}
+	
+	public void set(Pos p, Integer i) {
+		if( caselle.get(p)==null )
+			caselle.put(p, i) ;
+		else
+			throw new RuntimeException("Casella già occupata") ;
+	}
+	
+	public void delete(Pos p) {
+		if( caselle.get(p)!=null ) 
+			caselle.put(p, null) ;
+		else
+			throw new RuntimeException("Casella già vuota") ;
+	}
+	
+	public boolean valid(Pos p) {
+		return posizioni.contains(p) ;
+	}
+	
+	public int size() {
+		return posizioni.size() ;
+	}
+	
+	public List<Pos> getPosizioni() {
+		return posizioni ;
+	}
+	
+	
+
+	
+}
